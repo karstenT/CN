793d5ab47f9672e0599e59e282b2d1663fc21f7c
karstenT <tolle@comt-it.de>
2020-09-07 08:45:07 +0200

    Update nlp_list_person_helper.sql
    improve mapping and included not found "PERSON"

--- Python/NLP/nlp_list_person_helper.sql
+++ Python/NLP/nlp_list_person_helper.sql
@@ -14,6 +14,16 @@ COLLATE='utf8_general_ci'
 ENGINE=InnoDB
 ;
 
-
+# includes the mapping to alternativenames
 INSERT INTO nlp_list_person_helper(DesignID, NLPID)
-SELECT ner.DesignID, nlp.id FROM cnt_pipeline_ner ner, nlp_list_person nlp WHERE LABEL_ENTITY = 'PERSON' AND ner.entity = nlp.name;
+SELECT ner.DesignID, nlp.id FROM cnt_pipeline_ner ner, nlp_list_person nlp WHERE
+LABEL_ENTITY = 'PERSON' AND
+(ner.entity = nlp.name or INSTR(nlp.alternativenames, ner.entity) > 0); 
+
+# not found "PERSON" can be listed by:
+select distinct Entity from cnt_pipeline_ner ner WHERE
+LABEL_ENTITY = 'PERSON' and 
+Entity not in 
+(select distinct Entity from cnt_pipeline_ner, nlp_list_person nlp WHERE
+LABEL_ENTITY = 'PERSON' AND
+(ner.entity = nlp.name or INSTR(nlp.alternativenames, ner.entity) > 0));
